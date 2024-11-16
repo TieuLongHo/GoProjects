22ca3c35795eb5654459040176d582923854963b
Tieu Long <tieu.ho@students.bfh.ch>
2024-11-17 00:25:09 +0100

    add corse handling

--- Todo/backend/cmd/main.go
+++ Todo/backend/cmd/main.go
@@ -15,10 +15,27 @@ func main() {
 	repository.CreateTodo(models.TodoRequest{Description: "write documentation"})
 	repository.DeleteTodo(3)
 
-	http.HandleFunc("GET /todos", GetAllTodos)
-	http.HandleFunc("GET /todos/{id}", GetTodo)
-	http.HandleFunc("POST /todos", CreateTodo)
-	http.HandleFunc("PUT /todos/{id}", CheckTodo)
-	http.HandleFunc("DELETE /todos/{id}", DeleteTodo)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /todos", GetAllTodos)
+	mux.HandleFunc("GET /todos/{id}", GetTodo)
+	mux.HandleFunc("POST /todos", CreateTodo)
+	mux.HandleFunc("PUT /todos/{id}", CheckTodo)
+	mux.HandleFunc("DELETE /todos/{id}", DeleteTodo)
+	http.ListenAndServe(":8080", corsMiddleware(mux))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
